055ca4b7d048bd2adfbfe18829ae0f12698ef16f
ykosaka <ykosaka@student.42tokyo.jp>
2024-09-30 10:23:57 +0900

    Debugging mariadb

--- inception/srcs/mariadb/entrypoint.sh
+++ inception/srcs/mariadb/entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+cd '/usr'
+sed -i "/\(bind-address.*$\)/a bind-address            = $WP_DB_HOST" /etc/mysql/mariadb.conf.d/50-server.cnf 
+# mysql_install_db --user=mysql --ldata=/var/lib/mysql || mysql_upgrade -u root --password=$MYSQL_ROOT_PASSWORD
 mysql_install_db --user=mysql --ldata=/var/lib/mysql
 # mysqld_safe --skip-grant-tables &
-mysqld_safe &
+# mysqld_safe &
+/usr/bin/mariadbd-safe --datadir='/var/lib/mysql' &
+# service mariadb start
 
-mysql -u root < ./entrypoint.sql
+mysql -u root < /entrypoint.sql
 
-pkill mysqld
+# pkill mysqld
+# service mariadb stop
