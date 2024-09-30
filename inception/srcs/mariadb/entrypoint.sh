1c92eaf9fdc7781630da2622028f4fbd8e749edf
ykosaka <ykosaka@student.42tokyo.jp>
2024-09-30 13:19:54 +0900

    SSL

--- inception/srcs/mariadb/entrypoint.sh
+++ inception/srcs/mariadb/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+PATH_CONF='/etc/mysql/mariadb.conf.d/50-server.cnf'
 cd '/usr'
-sed -i "/\(bind-address.*$\)/a bind-address            = $WP_DB_HOST" /etc/mysql/mariadb.conf.d/50-server.cnf 
+grep "bind-address[ \t]*=[ \t]*$WP_DB_HOST" $PATH_CONF || sed -i "/\(bind-address.*$\)/a bind-address            = $WP_DB_HOST" $PATH_CONF 
+# sed -i "/\(bind-address.*$\)/a bind-address            = $IP_DB" $PATH_CONF 
 # mysql_install_db --user=mysql --ldata=/var/lib/mysql || mysql_upgrade -u root --password=$MYSQL_ROOT_PASSWORD
 mysql_install_db --user=mysql --ldata=/var/lib/mysql
 # mysqld_safe --skip-grant-tables &
