902391158bc7df4682972df5b3eb348c5e507a5d
Yoshihiro Kosaka <ykosaka@c4r1s1.42tokyo.jp>
2024-10-07 22:08:46 +0900

    add install-chrome.sh

---
+++ inception/install-chrome.sh
@@ -0,0 +1,4 @@
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/googlechrom-keyring.gpg] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
+curl -fsSL https://dl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor -o /usr/share/keyrings/googlechrom-keyring.gpg
+sudo apt update
+sudo apt install google-chrome-stable
