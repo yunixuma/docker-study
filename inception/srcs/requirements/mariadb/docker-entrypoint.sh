5fc05519b3b5379e70eae85117ded560b658af2f
ykosaka <ykosaka@student.42tokyo.jp>
2024-10-08 14:33:16 +0900

    Modify docker-entrypoint.sh (logging)

--- inception/srcs/requirements/mariadb/docker-entrypoint.sh
+++ inception/srcs/requirements/mariadb/docker-entrypoint.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# mkdir -p /var/lib/mysql
-# chown -hR 101:101 /var/lib/mysql
-# chmod 755 -R /var/lib/mysql/
-
 PATH_DB=/var/lib/mysql
-PATH_LOG=~/entrypoint.log
-echo "[`date +"%Y-%m-%d %H:%M:%S"`]\t$0" >> $PATH_LOG
+PATH_LOG=./`basename $0 .sh`.log
+echo "[`date +"%Y-%m-%d %H:%M:%S"`]\t$0" | tee -a $PATH_LOG
 
 echo "PATH_DB=$PATH_DB" >> $PATH_LOG
 echo "PATH_LOG=$PATH_LOG" >> $PATH_LOG
@@ -14,8 +10,8 @@ echo "MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD" >> $PATH_LOG
 echo "WP_DB_USER=$WP_DB_USER" >> $PATH_LOG
 
 cd /usr
-mysql_install_db --user=mysql --basedir=/usr --datadir=$PATH_DB >> $PATH_LOG 2>&1 \
-    || mysql_upgrade -u root --password=$MYSQL_ROOT_PASSWORD >> $PATH_LOG 2>&1
+mysql_install_db --user=mysql --basedir=/usr --datadir=$PATH_DB 2>&1 | tee -a $PATH_LOG \
+    || mysql_upgrade -u root --password=$MYSQL_ROOT_PASSWORD 2>&1 | tee -a $PATH_LOG
 
 # mysqld_safe --skip-grant-tables &
 # mysqld_safe &
@@ -23,12 +19,12 @@ mysql_install_db --user=mysql --basedir=/usr --datadir=$PATH_DB >> $PATH_LOG 2>&
 service mariadb start
 
 # if [ -f "/root/setup.sql" ]; then
-    mysql -u root < /root/setup.sql >> $PATH_LOG 2>&1
+    mysql -u root < /root/setup.sql 2>&1 | tee -a $PATH_LOG
     # rm /root/setup.sql
 # fi
 
 pkill mariadbd
 # service mariadb stop
-# mariadbd-safe -u dbuser --datadir=$PATH_DB >> PATH_LOG 2>&1
-mariadbd -u $WP_DB_USER --datadir=$PATH_DB >> PATH_LOG 2>&1
+# mariadbd-safe -u dbuser --datadir=$PATH_DB 2>&1 | tee -a $PATH_LOG
+mariadbd -u $WP_DB_USER --datadir=$PATH_DB 2>&1 | tee -a $PATH_LOG
 # tail -f /dev/null
--- inception/srcs/requirements/wordpress/docker-entrypoint.sh
+++ inception/srcs/requirements/wordpress/docker-entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 PATH_SITE=/var/www/html
-PATH_LOG=~/entrypoint.log
-date +"%Y-%m-%d %H:%M:%S" >> $PATH_LOG
-echo "[`date +"%Y-%m-%d %H:%M:%S"`]\t$0" >> $PATH_LOG
+PATH_LOG=./`basename $0 .sh`.log
+echo "[`date +"%Y-%m-%d %H:%M:%S"`]\t$0" | tee -a $PATH_LOG
 
 echo "PATH_SITE=$PATH_SITE" >> $PATH_LOG
 echo "PATH_LOG=$PATH_LOG" >> $PATH_LOG
@@ -23,18 +22,18 @@ echo "WP_SUB_PASSWORD=$WP_SUB_PASSWORD" >> $PATH_LOG
 cd $PATH_SITE
 # ls wp-login.php
 if [ ! -f "wp-login.php" ]; then
-	chmod 755 ./ 2>> $PATH_LOG
+	chmod 755 ./ 2>&1 | tee -a $PATH_LOG
 	# mysql -u $WP_DB_USER --password=$WP_DB_PASSWORD --host $WP_DB_HOST --connect-timeout 300
-	wp core download --path="$PATH_SITE" --allow-root >> $PATH_LOG 2>&1
-	wp core config --dbname="$WP_DB_NAME" --dbuser="$WP_DB_USER" --dbpass="$WP_DB_PASSWORD" --dbhost="$WP_DB_HOST" --path="$PATH_SITE" --allow-root >> $PATH_LOG 2>&1
-	wp core install --url="http://$WP_HOME/" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --path="/var/www/html" --allow-root >> $PATH_LOG 2>&1
-	wp user create "$WP_SUB_USER" "$WP_SUB_EMAIL" --role=author --user_pass="$WP_SUB_PASSWORD" --allow-root >> $PATH_LOG 2>&1
+	wp core download --path="$PATH_SITE" --allow-root 2>&1 | tee -a $PATH_LOG
+	wp core config --dbname="$WP_DB_NAME" --dbuser="$WP_DB_USER" --dbpass="$WP_DB_PASSWORD" --dbhost="$WP_DB_HOST" --path="$PATH_SITE" --allow-root 2>&1 | tee -a $PATH_LOG
+	wp core install --url="http://$WP_HOME/" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --path="/var/www/html" --allow-root 2>&1 | tee -a $PATH_LOG
+	wp user create "$WP_SUB_USER" "$WP_SUB_EMAIL" --role=author --user_pass="$WP_SUB_PASSWORD" --allow-root 2>&1 | tee -a $PATH_LOG
 	# wp plugin install --activate --allow-root
-	rm -f index.htm* >> $PATH_LOG 2>&1
+	rm -f index.htm* 2>&1 | tee -a $PATH_LOG
 	chown -R www-data:www-data ./ 2>> $PATH_LOG
 	find ./ -type d -exec chmod 755 {} + 2>> $PATH_LOG
 	find ./ -type f -exec chmod 644 {} + 2>> $PATH_LOG
 fi
 
-php -S 0.0.0.0:443 -t /var/www/html >> $PATH_LOG 2>&1 &
-php-fpm7.4 -F >> $PATH_LOG 2>&1
+php -S 0.0.0.0:443 -t /var/www/html 2>&1 | tee -a $PATH_LOG &
+php-fpm7.4 -F 2>&1 | tee -a $PATH_LOG
